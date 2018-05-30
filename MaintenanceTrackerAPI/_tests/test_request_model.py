3d9718ba7a1fd0d858a317d527800e01ac0ef0e9
Dave Mathews <davenmathews@gmail.com>
2018-05-30 19:04:26 +0300

    add more tests for input validation in Request creation
    Signed-off-by: Dave Mathews <davenmathews@gmail.com>

--- MaintenanceTrackerAPI/_tests/test_request_model.py
+++ MaintenanceTrackerAPI/_tests/test_request_model.py
@@ -25,3 +25,59 @@ class RequestTestCase(unittest.TestCase):
                     'Water spilled onto my keyboard. I need it replaced')
         exception = a.exception
         self.assertEqual('Administrators cannot make requests!', exception.msg)
+
+    def test_make_request_with_invalid_title(self):
+        with self.assertRaises(RequestTransactionError) as a:
+            Request(self.consumer, 'Repair', '############',
+                    'Water spilled onto my keyboard. I need it replaced')
+        exception = a.exception
+        self.assertEqual('please enter a valid title', exception.msg)
+
+    def test_make_request_with_short_title(self):
+        with self.assertRaises(RequestTransactionError) as a:
+            Request(self.consumer, 'Repair', 'XX',
+                    'Water spilled onto my keyboard. I need it replaced')
+        exception = a.exception
+        self.assertEqual('title too short. Min of 10 characters allowed', exception.msg)
+
+    def test_make_request_with_wrong_spacing_title(self):
+        with self.assertRaises(RequestTransactionError) as a:
+            Request(self.consumer, 'Repair', 'X                     X',
+                    'Water spilled onto my keyboard. I need it replaced')
+        exception = a.exception
+        self.assertEqual('Please check the spacing on your title', exception.msg)
+
+    def test_make_request_with_wrong_punctuation_title(self):
+        with self.assertRaises(RequestTransactionError) as a:
+            Request(self.consumer, 'Repair', 'Aka..\'.\';.\'.;\'.',
+                    'Water spilled onto my keyboard. I need it replaced')
+        exception = a.exception
+        self.assertEqual('please check the punctuation in your title', exception.msg)
+
+    def test_make_request_with_invalid_description(self):
+        with self.assertRaises(RequestTransactionError) as a:
+            Request(self.consumer, 'Repair', 'Laptop Repair',
+                    '###########################################')
+        exception = a.exception
+        self.assertEqual('please enter a valid description', exception.msg)
+
+    def test_make_request_with_short_description(self):
+        with self.assertRaises(RequestTransactionError) as a:
+            Request(self.consumer, 'Repair', 'Laptop Repair',
+                    'W')
+        exception = a.exception
+        self.assertEqual('description too short. Min of 40 characters allowed', exception.msg)
+
+    def test_make_request_with_wrong_spacing_description(self):
+        with self.assertRaises(RequestTransactionError) as a:
+            Request(self.consumer, 'Repair', 'Laptop Repair',
+                    'W               lled onto my keyboard.      I need it replaced')
+        exception = a.exception
+        self.assertEqual('Please check the spacing on your description', exception.msg)
+
+    def test_make_request_with_wrong_punctuation_description(self):
+        with self.assertRaises(RequestTransactionError) as a:
+            Request(self.consumer, 'Repair', 'Laptop Repair',
+                    'Water spilled onto my Aka..\'.\';.\'.;\'. I need it reAka..\'.\';.\'.;\'.placed')
+        exception = a.exception
+        self.assertEqual('please check the punctuation in your description', exception.msg)
