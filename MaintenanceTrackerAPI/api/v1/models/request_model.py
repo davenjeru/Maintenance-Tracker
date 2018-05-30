452fc783755a2e5b3d8d3b13a0610c0f68b2ebbd
Dave Mathews <davenmathews@gmail.com>
2018-05-30 19:14:55 +0300

    add checks for short title and description
    tests passed:
    - test_make_request_with_short_description
    - test_make_request_with_short_title
    
    Signed-off-by: Dave Mathews <davenmathews@gmail.com>

--- MaintenanceTrackerAPI/api/v1/models/request_model.py
+++ MaintenanceTrackerAPI/api/v1/models/request_model.py
@@ -30,8 +30,15 @@ class Request(object):
     id = 1
 
     def __init__(self, user: Consumer, request_type: str, title: str, description: str):
+        # Return 403 (Forbidden!) when Administrator tries to make a request
         if user.role != 'Consumer':
             raise RequestTransactionError('Administrators cannot make requests!', 403)
+
+        try:
+            self.__validate_request_details('title', title)
+            self.__validate_request_details('description', description)
+        except AssertionError as a:
+            raise RequestTransactionError(a.args[0])
 
         self.id = Request.id
         self.user_id = user.id
@@ -41,3 +48,29 @@ class Request(object):
         self.status = 'Pending Approval'
         self.date_requested = datetime.datetime.now()
         self.last_modified = None
+        self.__save()
+
+    def __save(self):
+        Request.id += 1
+        requests_list.append(self)
+
+    @staticmethod
+    def __validate_request_details(name: str, item: str):
+        """
+            Used to validate title or description depending on the context given
+            :param name: the context of validation
+            :param item: item to be validated
+
+        """
+        max_length, min_length = None, None
+
+        if name == 'title':
+            max_length = 70
+            min_length = 10
+        elif name == 'description':
+            max_length = 250
+            min_length = 40
+        if len(item) > max_length:
+            raise AssertionError('{0} too long. Max of {1} characters allowed'.format(name, max_length))
+        if len(item) < min_length:
+            raise AssertionError('{0} too short. Min of {1} characters allowed'.format(name, min_length))
