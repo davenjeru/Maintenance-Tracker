1be082a30735cef3b9734fe03e849c34acd06636
Dave Mathews <davenmathews@gmail.com>
2018-05-30 16:55:39 +0300

    add tests for creating the Request class
    tests added include:
    - test_consumer_makes_request
    - test_admin_makes_request
    
    update .travis.yml to reflect the cover-package as the models package
    
    Signed-off-by: Dave Mathews <davenmathews@gmail.com>

---
+++ MaintenanceTrackerAPI/_tests/test_request_model.py
@@ -0,0 +1,26 @@
+import unittest
+
+from MaintenanceTrackerAPI.api.v1.models.request_model import Request, RequestTransactionError, requests_list
+from MaintenanceTrackerAPI.api.v1.models.user_model import Consumer, Admin
+
+
+class RequestTestCase(unittest.TestCase):
+
+    def setUp(self):
+        requests_list.clear()
+        self.consumer = Consumer('consumer@company.com', 'password.Pa55word', 'What is your favourite company?',
+                                 'company')
+        self.admin = Admin('admin@company.com', 'password.Pa55word', 'What is your favourite company?',
+                           'company')
+
+    def test_consumer_makes_request(self):
+        request = Request(self.consumer, 'Repair', 'Laptop Repair',
+                          'Water spilled onto my keyboard. I need it replaced')
+        self.assertEqual(request.user_id, self.consumer.id)
+
+    def test_admin_makes_request_fail(self):
+        with self.assertRaises(RequestTransactionError) as a:
+            Request(self.admin, 'Repair', 'Laptop Repair',
+                    'Water spilled onto my keyboard. I need it replaced')
+        exception = a.exception
+        self.assertEqual('Administrators cannot make requests!', exception.msg)
