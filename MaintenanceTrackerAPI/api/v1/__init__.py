470715d54f46c9faff0e3fbe6fdd3affb39d46f0
Dave Mathews <davenmathews@gmail.com>
2018-05-31 10:25:25 +0300

    delete the users namespace
    - change cover package to '*/api' in .travis.yml
    - add space between test coverage badge and
      maintainability badge
    
    Signed-off-by: Dave Mathews <davenmathews@gmail.com>

--- MaintenanceTrackerAPI/api/v1/__init__.py
+++ MaintenanceTrackerAPI/api/v1/__init__.py
@@ -1,7 +1,6 @@
 from flask import Blueprint
 from flask_restplus import Api
 
-from MaintenanceTrackerAPI.api.v1.users import users_ns
 
 # initiate blueprint
 api_v1_blueprint = Blueprint('apiV1', __name__, url_prefix='/api/v1')
@@ -18,6 +17,5 @@ del api_v1.namespaces[0]
 api_v1.catch_all_404s = True
 
 # register namespaces related to this api
-api_v1.add_namespace(users_ns)
 
 # register extensions
