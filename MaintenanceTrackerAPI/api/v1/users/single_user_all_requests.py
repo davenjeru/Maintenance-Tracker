f8e694a96b2c7dcc84cae9bd30b650c1908ad1d2
Dave Mathews <davenmathews@gmail.com>
2018-05-31 13:16:27 +0300

    fix typos in the request creation route
    Signed-off-by: Dave Mathews <davenmathews@gmail.com>

--- MaintenanceTrackerAPI/api/v1/users/single_user_all_requests.py
+++ MaintenanceTrackerAPI/api/v1/users/single_user_all_requests.py
@@ -11,18 +11,18 @@ class SingleUserAllRequests(Resource):
         pass
 
     @login_required
-    @users_ns.response(201, 'Post created successfully')
+    @users_ns.response(201, 'Request made successfully')
     @users_ns.response(400, 'Bad request')
     @users_ns.response(401, 'Not logged in hence unauthorized')
     @users_ns.response(403, 'Logged in but forbidden from performing this action')
     def post(self, user_id: int):
         """
-        Add a post
+        Make a request
 
-        1. User must be logged in to create a post
+        1. User must be logged in to make a request
         2. Your title should have between 10 and 70 characters
-        3. Your body should have between 40 and 500 characters
-        4. Duplicate posts will not be created
+        3. Your body should have between 40 and 250 characters
+        4. Duplicate requests will not be created
 
         """
         pass
