61b46eab2a0c69c13639c30403113efb6eafec48
Dave Mathews <davenmathews@gmail.com>
2018-05-31 11:51:57 +0300

    add functionality for logging in
    Signed-off-by: Dave Mathews <davenmathews@gmail.com>

--- MaintenanceTrackerAPI/api/v1/auth/login.py
+++ MaintenanceTrackerAPI/api/v1/auth/login.py
@@ -1,12 +1,21 @@
-from flask_login import LoginManager
-from flask_restplus import Resource
+from flask_login import LoginManager, current_user, login_user
+from flask_restplus import Resource, fields
 from flask_restplus.namespace import Namespace
 
+from MaintenanceTrackerAPI.api.v1.boilerplate import generate_auth_output, PayloadExtractionError, extract_from_payload, \
+    get_validated_payload
 from MaintenanceTrackerAPI.api.v1.models.user_model import users_list
 
 auth_ns = Namespace('auth')
 
 login_manager = LoginManager()
+
+user_login_model = auth_ns.model('user_login_model', {
+    'email': fields.String(title='Your email address', required=True,
+                           example='myemail@company.com'),
+    'password': fields.String(title='Your email address', required=True,
+                              example='password.Pa55word')
+})
 
 
 @login_manager.user_loader
@@ -19,6 +28,11 @@ def load_user(user_id):
 
 
 class Login(Resource):
+    @auth_ns.expect(user_login_model)
+    @auth_ns.response(200, 'user logged in successfully')
+    @auth_ns.response(415, 'request data not in json format')
+    @auth_ns.response(401, 'invalid password')
+    @auth_ns.response(400, 'bad request')
     def post(self):
         """
         User Login
@@ -30,4 +44,27 @@ class Login(Resource):
         Note: Only one user can be logged in per client
 
         """
-        pass
+        try:
+            return {'message': current_user.email + ' is currently logged in'}, 400
+        except AttributeError:
+            pass
+
+        email, password = None, None
+        try:
+            payload = get_validated_payload(self)
+            list_of_names = ['email', 'password']
+            email, password = extract_from_payload(payload, list_of_names)
+        except PayloadExtractionError as e:
+            auth_ns.abort(e.abort_code, e.msg)
+
+        for a_user in users_list:
+            if email == a_user.email:
+                if a_user.authenticate(password):
+                    login_user(a_user)
+                    output = generate_auth_output(self, a_user)
+                    response = self.api.make_response(output, 200)
+                    return response
+                else:
+                    auth_ns.abort(401, 'invalid password')
+        else:
+            auth_ns.abort(400, 'user not found!')
