a5a7a3707a9ba143aae2543bfdf0d7e05af28df8
Dave Mathews <davenmathews@gmail.com>
2018-05-31 17:21:03 +0300

    add docstrings to the users model tests
    Signed-off-by: Dave Mathews <davenmathews@gmail.com>

--- MaintenanceTrackerAPI/_tests/test_user_model.py
+++ MaintenanceTrackerAPI/_tests/test_user_model.py
@@ -5,6 +5,11 @@ from MaintenanceTrackerAPI.api.v1.models.user_model import User, UserTransaction
 
 class UserBaseTestCase(unittest.TestCase):
     def setUp(self):
+        """
+        Overridden method from unittest.TestCase class
+
+        :return: None
+        """
         users_list.clear()
         self.arguments = dict(email='email@company.com', password='password.Pa55word',
                               security_question='What is your favourite company?',
@@ -13,6 +18,11 @@ class UserBaseTestCase(unittest.TestCase):
         self.u = User('email@company.com', 'password.Pa55word', 'What is your favourite company?', 'company')
 
     def tearDown(self):
+        """
+        Overridden method from unittest.TestCase class
+
+        :return: None
+        """
         users_list.clear()
 
     def expect_user_transaction_error(self, expected_error_message: str, the_callable: callable, arguments: dict):
@@ -33,38 +43,66 @@ class UserBaseTestCase(unittest.TestCase):
 class ModelsTestCase(UserBaseTestCase):
 
     def test_create_user_pass(self):
+        """
+        Test that a user can be created
+        :return: None
+        """
         self.assertEqual('email@company.com', self.u.email)
         self.assertTrue(self.u.authenticate('password.Pa55word'))
 
     def test_create_user_with_wrong_email_syntax(self):
+        """
+        Test that a user cannot be created with invalid email value
+        :return: None
+        """
         # test user creation with wrong email syntax
         self.arguments['email'] = 'email@com'
         expected_error_message = 'email address syntax is invalid'
         self.expect_user_transaction_error(expected_error_message, User, self.arguments)
 
     def test_create_user_with_wrong_password_syntax(self):
+        """
+        Test that a user cannot be created with invalid password syntax
+        :return: None
+        """
         # test user creation with wrong password syntax
         self.arguments['password'] = 'password'
         expected_error_message = 'password syntax is invalid'
         self.expect_user_transaction_error(expected_error_message, User, self.arguments)
 
     def test_create_user_with_wrong_security_question_syntax(self):
+        """
+        Test that a user cannot be created with wrong security question syntax
+        :return: None
+        """
         # test user creation with wrong security question syntax
         self.arguments['security_question'] = 'question'
         expected_error_message = 'security question must start with a \'Wh\' or a \'Are\' question'
         self.expect_user_transaction_error(expected_error_message, User, self.arguments)
 
     def test_create_user_with_wrong_security_answer_syntax(self):
+        """
+        Test that a user cannot be created with wrong security answer syntax
+        :return: None
+        """
         # test user creation with wrong security answer syntax
         self.arguments['security_answer'] = 'smm.vf....ff.,'
         expected_error_message = 'security answer must not contain any punctuations'
         self.expect_user_transaction_error(expected_error_message, User, self.arguments)
 
     def test_user_reset_password_pass(self):
+        """
+        Test that a user can reset their password
+        :return: None
+        """
         self.u.reset_password('What is your favourite company?', 'company', 'another.Pa55word')
         self.assertTrue(self.u.authenticate('another.Pa55word'))
 
     def test_user_reset_password_with_wrong_security_question(self):
+        """
+        Test that user cannot reset password with wrong security question
+        :return: None
+        """
         # reset password with wrong security question
         with self.assertRaises(UserTransactionError) as a:
             self.u.reset_password('favourite company?', 'company', 'another.Pa55word')
@@ -72,6 +110,10 @@ class ModelsTestCase(UserBaseTestCase):
         self.assertEqual('wrong security question!', exception.msg)
 
     def test_user_reset_password_with_wrong_security_answer(self):
+        """
+        Test that user cannot reset password with wrong security answer
+        :return: None
+        """
         # reset password with wrong security answer
         with self.assertRaises(UserTransactionError) as a:
             self.u.reset_password('What is your favourite company?', 'no answer', 'another.Pa55word')
@@ -79,11 +121,19 @@ class ModelsTestCase(UserBaseTestCase):
         self.assertEqual('wrong security answer!', exception.msg)
 
     def test_create_admin_user_pass(self):
+        """
+        Test that an Administrator can be created
+        :return: None
+        """
         admin = Admin('admin@company.com', 'password.Pa55word', 'What is your favourite company?',
                       'company')
         self.assertEqual('Administrator', admin.role)
 
     def test_create_consumer_user_pass(self):
+        """
+        Test that a Consumer can be created
+        :return: None
+        """
         consumer = Consumer('consumer@company.com', 'password.Pa55word', 'What is your favourite company?',
                             'company')
         self.assertEqual('Consumer', consumer.role)
