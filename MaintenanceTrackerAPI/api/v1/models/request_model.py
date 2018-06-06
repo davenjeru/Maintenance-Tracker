b6969a6f6e6614d5669ddccfc5d92ae8c875a810
Dave Mathews <davenmathews@gmail.com>
2018-06-06 22:28:50 +0300

    Chore #158156210 Modify Request Model
    - modify request model to use database
    
    Signed-off-by: Dave Mathews <davenmathews@gmail.com>

--- MaintenanceTrackerAPI/api/v1/models/request_model.py
+++ MaintenanceTrackerAPI/api/v1/models/request_model.py
@@ -1,28 +1,29 @@
 import datetime
 import string
 
+from MaintenanceTrackerAPI.api.v1.database import Database
 from MaintenanceTrackerAPI.api.v1.exceptions import RequestTransactionError
-from .user_model import Consumer, Admin
 
-requests_list = []
+db = Database()
 
 
 class Request(object):
     """
     This class handles the request object and all its functions.
 
-    :param: user -> a user object. :param: request_type -> the type of
-    request, either 'Maintenance' or 'Repair' :param: title -> a short title
-    that summarizes the object(s) being repaired or maintained :param:
-    description -> a brief description of what is supposed to be repaired or
+    :param: user -> a user dictionary.
+    :param: request_type -> the type of request, either 'Maintenance'
+     or 'Repair'
+    :param: title -> a short title that summarizes the object(s)
+     being repaired or maintained
+    :param: description -> a brief description of what is supposed to be repaired or
     maintained
     """
-    id = 1
 
-    def __init__(self, user: Consumer, request_type: str,
+    def __init__(self, user: dict, request_type: str,
                  title: str, description: str):
         # Return 403 (Forbidden!) when Administrator tries to make a request
-        if user.role != 'Consumer':
+        if user['role'] != 'Consumer':
             raise RequestTransactionError(
                 'Administrators cannot make requests!', 403)
 
@@ -38,21 +39,25 @@ class Request(object):
         except AssertionError as a:
             raise RequestTransactionError(a.args[0])
 
-        for request in requests_list:
-            if request.title == title and request.description == description:
-                raise RequestTransactionError('similar request exists')
+        db.cur = db.conn.cursor()
+        sql = 'select(title, description) from requests where (title =%s and' \
+              ' description=%s)'
+        data = (title, description)
+        try:
+            db.cur.execute(sql, data)
+        except Exception:
+            pass
         # TODO check for the request status to allow re-submission
         # of a request that has already been resolved/rejected
 
-        self.id = Request.id
-        self.user_id = user.id
+        self.user_id = user['user_id']
         self.type = request_type if (bool(request_type)) else 'Repair'
         self.title = title
         self.description = description
         self.status = 'Pending Approval'
         self.date_requested = datetime.datetime.now()
         self.last_modified = None
-        self.__requested_by = user.email
+        self.requested_by = user['email']
         self.__save()
 
     def __save(self):
@@ -60,8 +65,7 @@ class Request(object):
         Saves the instance by adding it to a list and increases the id by one
         :return:
         """
-        Request.id += 1
-        requests_list.append(self)
+        db.save_request(self)
 
     @staticmethod
     def __validate_request_details(name: str, item: str):
@@ -101,6 +105,10 @@ class Request(object):
         item_words = item.split(' ')
 
         for word in item_words:
+            if word.count('.') > 3:
+                raise AssertionError(
+                    'please check the punctuation in your {}'.format(name))
+
             if not word:  # this means there is extra space
                 raise AssertionError('Please check the'
                                      ' spacing on your {}'.format(name))
@@ -116,214 +124,3 @@ class Request(object):
                             and char_list[i] != '.':
                         raise AssertionError('please check the punctuation in'
                                              ' your {}'.format(name))
-
-    def approve(self, user: Admin):
-        """
-        For changing the request status to 'Approved'
-        This can only be done to a request whose status is 'Pending Approval'
-        :param user: An Admin User object
-        :return: None
-        """
-
-        # check the role of the user
-        self.__check_for_admin(user)
-
-        # check the status of the request
-        if self.status != 'Pending Approval':
-            raise RequestTransactionError('cannot approve a request'
-                                          ' which is {}'.format(self.status))
-
-        self.status = 'Approved'
-        self.last_modified = datetime.datetime.now()
-
-    def reject(self, user: Admin):
-        """
-        For changing the request status to 'Rejected'
-        This can only be done to a request whose status is 'Pending Approval'
-        :param user: An Admin User object
-        :return: None
-        """
-
-        # check the role of the user
-        self.__check_for_admin(user)
-
-        # check the status of the request
-        if self.status != 'Pending Approval':
-            raise RequestTransactionError('cannot reject a request which is'
-                                          ' {}'.format(self.status))
-
-        self.status = 'Rejected'
-        self.last_modified = datetime.datetime.now()
-
-    def in_progress(self, user: Admin):
-        """
-        For changing the request status to 'In Progress'
-        This can only be done to a request whose status is 'Approved'
-        :param user: An Admin User object
-        :return: None
-        """
-
-        # check the role of the user
-        self.__check_for_admin(user)
-
-        # check the status of the request
-        if self.status != 'Approved':
-            raise RequestTransactionError('cannot in progress a request which'
-                                          ' is {}'.format(self.status))
-
-        self.status = 'In Progress'
-        self.last_modified = datetime.datetime.now()
-
-    def resolve(self, user: Admin):
-        """
-        For changing the request status to 'Resolved'
-        This can only be done to a request whose status is 'In Progress'
-        :param user: An Admin User object
-        :return: None
-        """
-
-        self.__check_for_admin(user)
-
-        # check the status of the request
-        if self.status != 'In Progress':
-            raise RequestTransactionError('cannot resolve a request which is'
-                                          ' {}'.format(self.status))
-
-        self.status = 'Resolved'
-        self.last_modified = datetime.datetime.now()
-
-    def cancel(self, user: Consumer):
-        """
-        For changing the request status to 'Cancelled'
-        This can only be done to a request whose status is 'Pending Approval'
-        :param user: A Consumer User object
-        :return: None
-        """
-
-        # check the role of the user
-        self.__check_for_consumer(user, 'cancel')
-
-        # check the status of the request
-        if self.status != 'Pending Approval':
-            raise RequestTransactionError('cannot cancel a request which is'
-                                          ' {}'.format(self.status))
-
-        self.status = 'Cancelled'
-        self.last_modified = datetime.datetime.now()
-
-    def edit(self, user: Consumer, details: dict):
-        """
-        Method used by a consumer to edit a request
-        :param user: A Consumer User object
-        :param details: A dictionary containing the details to be changed
-        :return:
-        """
-        # check the role of the user
-        self.__check_for_consumer(user, 'edit')
-
-        # check whether this request belongs to the user given
-        if user.id != self.user_id:
-            raise RequestTransactionError('this request does not belong to'
-                                          ' the selected user', 403)
-
-        # check the request status
-        if self.status != 'Pending Approval':
-            raise RequestTransactionError('cannot edit a request which'
-                                          ' is {}'.format(self.status))
-
-        title = details.get('title', None)
-        description = details.get('description', None)
-
-        try:
-            if title is not None:
-                self.__validate_request_details('title', title)
-            if description is not None:
-                self.__validate_request_details('description', description)
-        except AssertionError as e:
-            raise RequestTransactionError(e.args[0])
-
-        if title is not None:
-            if self.title == title:
-                name = 'title'
-                raise RequestTransactionError('{0} given matches'
-                                              ' the previous {0}'.format(name))
-        elif description is not None:
-            if self.description == description:
-                name = 'description'
-                raise RequestTransactionError('{0} given matches the'
-                                              ' previous {0}'.format(name))
-
-        for request in requests_list:
-            if description is not None and request.description == description:
-                raise RequestTransactionError('similar description exists')
-
-        self.title = title if title is not None else self.title
-        self.description = description if description is not None \
-            else self.description
-
-        if not title and not description:
-            raise RequestTransactionError('could not edit request, please '
-                                          'insert title or description')
-        self.last_modified = datetime.datetime.now()
-        return self
-
-    def delete(self, user: Consumer):
-        """
-        Method used by Consumer to delete a request
-        :param user: Consumer User Object
-        :return: None
-        """
-        # check the role of the user
-        self.__check_for_consumer(user, 'delete')
-
-        # check the status of the request
-        if self.status != 'Resolved' and self.status != 'Rejected' \
-                and self.status != 'Cancelled':
-            raise RequestTransactionError('cannot delete a request which '
-                                          'is {}'.format(self.status))
-
-        requests_list.remove(self)
-        del self
-        return True
-
-    @property
-    def serialize(self):
-        """
-        :rtype: dict
-        :return: a dictionary containing the request details
-        """
-        return dict(requested_by=self.requested_by, request_type=self.type,
-                    title=self.title, description=self.description,
-                    date_requestsed=str(self.date_requested),
-                    status=self.status, last_modified=str(self.last_modified))
-
-    @property
-    def requested_by(self):
-        """
-        :rtype: str
-        :return: The email address of the consumer who made the request
-        """
-        return self.__requested_by
-
-    @staticmethod
-    def __check_for_admin(user):
-        """
-        Private method for checking if the given user is an Administrator
-        :param user: User Object
-        :return: None
-        """
-        if user.role != 'Administrator':
-            raise RequestTransactionError('{} not allowed to change request'
-                                          ' status'.format(user.role))
-
-    @staticmethod
-    def __check_for_consumer(user, context: str):
-        """
-        Private method for checking if the given user is an Consumer
-        :param user: User object
-        :param context: The action that the user is trying to do
-        :return:
-        """
-        if user.role != 'Consumer':
-            raise RequestTransactionError('{0} not allowed to {1}'
-                                          ' request'.format(user.role, context))
