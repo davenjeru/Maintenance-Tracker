979a2dadaf2787f0034831668620ca7587b6dfc5
Dave Mathews <davenmathews@gmail.com>
2018-06-06 22:41:57 +0300

    Chore #158157143 Resolve Import Errors
    - resolve import and reference errors in login.py
    
    Signed-off-by: Dave Mathews <davenmathews@gmail.com>

--- MaintenanceTrackerAPI/api/v1/auth/login.py
+++ MaintenanceTrackerAPI/api/v1/auth/login.py
@@ -1,11 +1,10 @@
-from flask_login import LoginManager, current_user, login_user
+from flask_login import LoginManager, current_user
 from flask_restplus import Resource, fields
 from flask_restplus.namespace import Namespace
 
-from MaintenanceTrackerAPI.api.v1.boilerplate import generate_auth_output, \
-    extract_from_payload, get_validated_payload
+from MaintenanceTrackerAPI.api.v1.boilerplate import extract_from_payload, \
+    get_validated_payload
 from MaintenanceTrackerAPI.api.v1.exceptions import PayloadExtractionError
-from MaintenanceTrackerAPI.api.v1.models.user_model import users_list
 
 auth_ns = Namespace('auth')
 
@@ -17,21 +16,6 @@ user_login_model = auth_ns.model('user_login_model', {
     'password': fields.String(title='Your email address', required=True,
                               example='password.Pa55word')
 })
-
-
-@login_manager.user_loader
-def load_user(user_id):
-    """
-    This is the method used by Flask-Login to load a user using the user id
-    :param user_id: the user's id
-    :return: User object
-    """
-    for a_user in users_list:
-        # In the session, user_id is stored as a unicode character
-        # The chr() converts the int id of the user found to unicode
-        #  for comparing equality
-        if chr(a_user.id) == user_id:
-            return a_user
 
 
 class Login(Resource):
@@ -66,14 +50,4 @@ class Login(Resource):
         except PayloadExtractionError as e:
             auth_ns.abort(e.abort_code, e.msg)
 
-        for a_user in users_list:
-            if email == a_user.email:
-                if a_user.authenticate(password):
-                    login_user(a_user)
-                    output = generate_auth_output(self, a_user)
-                    response = self.api.make_response(output, 200)
-                    return response
-                else:
-                    auth_ns.abort(401, 'invalid password')
-        else:
-            auth_ns.abort(400, 'user not found!')
+        pass
