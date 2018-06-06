8157fb0f8025103bf4596e272075f456c160e6c8
Dave Mathews <davenmathews@gmail.com>
2018-06-06 20:23:30 +0300

    create function to save a user to the database
    Signed-off-by: Dave Mathews <davenmathews@gmail.com>

--- MaintenanceTrackerAPI/api/v1/database.py
+++ MaintenanceTrackerAPI/api/v1/database.py
@@ -37,7 +37,8 @@ class Database:
                              "email varchar UNIQUE NOT NULL," \
                              "password_hash varchar NOT NULL," \
                              "security_question varchar NOT NULL," \
-                             "security_answer_hash varchar NOT NULL);"
+                             "security_answer_hash varchar NOT NULL," \
+                             "role varchar NOT NULL DEFAULT 'Consumer');"
         self.query(create_users_table)
         self.conn.commit()
 
@@ -66,4 +67,41 @@ class Database:
         self.query("drop table requests;")
         self.query("drop table users;")
         self.query("drop table tokens;")
+        self.conn.commit()
+
+    def get_user_by_email(self, email: str):
+        """
+        Returns a dictionary with the user details if a user is found.
+        Returns None otherwise
+        :param email: the email to search with
+        :rtype: dict
+        """
+        query = "select * from users where email={}".format("'" + email + "'")
+        self.query(query)
+        item = self.cur.fetchone()
+        if not item:
+            user = None
+        else:
+            user = dict(
+                user_id=item[0],
+                email=item[1],
+                password_hash=item[2],
+                security_question=item[3],
+                security_answer_hash=item[4],
+                role=item[5]
+            )
+        return user
+
+    def save_user(self, user):
+        email = user.email
+        password_hash = user.password_hash
+        security_question = user.security_question
+        security_answer_hash = user.security_answer_hash
+        role = user.role
+        sql = 'insert into users(email, password_hash, security_question,' \
+              ' security_answer_hash, role) values(%s, %s, %s, %s, %s)'
+        data = (email, password_hash, security_question, security_answer_hash
+                , role)
+        self.cur = self.conn.cursor()
+        self.cur.execute(sql, data)
         self.conn.commit()
