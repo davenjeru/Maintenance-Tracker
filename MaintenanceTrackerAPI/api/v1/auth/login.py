09f8fa01eaea3d5039677c28d2b37a44fe82c13d
Dave Mathews <davenmathews@gmail.com>
2018-06-07 17:59:39 +0300

    [Chore #15814085] User Login(db)
    - add code to implement user login with JWT
    
    [Finishes #158140285]
    
    Signed-off-by: Dave Mathews <davenmathews@gmail.com>

--- MaintenanceTrackerAPI/api/v1/auth/login.py
+++ MaintenanceTrackerAPI/api/v1/auth/login.py
@@ -1,12 +1,15 @@
-from flask_jwt_extended import JWTManager
+from flask_jwt_extended import JWTManager, create_access_token
 from flask_restplus import Resource, fields
 from flask_restplus.namespace import Namespace
+from werkzeug.security import check_password_hash
 
 from MaintenanceTrackerAPI.api.v1.boilerplate import extract_from_payload, \
-    get_validated_payload
+    get_validated_payload, generate_auth_output
+from MaintenanceTrackerAPI.api.v1.database import Database
 from MaintenanceTrackerAPI.api.v1.exceptions import PayloadExtractionError
 
 jwt = JWTManager()
+db = Database()
 
 auth_ns = Namespace('auth')
 
@@ -46,4 +49,22 @@ class Login(Resource):
         except PayloadExtractionError as e:
             auth_ns.abort(e.abort_code, e.msg)
 
-        pass
+        # get user from the database
+        this_user = db.get_user_by_email(email)
+
+        # return user not found if None
+        if not this_user:
+            auth_ns.abort(404, 'User not found')
+
+        # check if the password is okay
+        if not check_password_hash(this_user['password_hash'], password):
+            auth_ns.abort(401, 'invalid password')
+
+        # password is valid so we give them an access token.
+        output = generate_auth_output(self, this_user)
+        identity = dict(user_id=this_user['user_id'],
+                        email=this_user['email'],
+                        role=this_user['role'])
+        output['access_token'] = create_access_token(identity)
+        response = self.api.make_response(output, 200)
+        return response
