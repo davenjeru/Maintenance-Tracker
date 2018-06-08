fe99c9e8bdeb5818231b13f3adb1dc93fb926137
Dave Mathews <davenmathews@gmail.com>
2018-06-08 05:22:19 +0300

    [Feature #158205567] Admin View One Request
    [Finishes #158205567]
    
    Signed-off-by: Dave Mathews <davenmathews@gmail.com>

--- MaintenanceTrackerAPI/api/v1/requests/single_request.py
+++ MaintenanceTrackerAPI/api/v1/requests/single_request.py
@@ -1,9 +1,28 @@
+from flask_jwt_extended import jwt_required, current_user
 from flask_restplus import Resource
 from flask_restplus.namespace import Namespace
 
+from MaintenanceTrackerAPI.api.v1.database import Database
+
 requests_ns = Namespace('requests')
+
+db = Database()
 
 
 class SingleRequest(Resource):
-    def get(self):
-        pass
+
+    @jwt_required
+    @requests_ns.doc(security='access_token')
+    @requests_ns.response(200, 'Request retrieved successfully')
+    @requests_ns.response(400, 'Bad request')
+    @requests_ns.response(401, 'Not logged in hence unauthorized')
+    @requests_ns.response(403, 'Logged in but forbidden from viewing '
+                               'these requests')
+    def get(self, request_id):
+        if current_user['role'] != 'Administrator':
+            requests_ns.abort(403)
+
+        request = db.get_request_by_id(request_id)
+        output = dict(request=request)
+        response = self.api.make_response(output, 200)
+        return response
