e05ac804f5c5ac0eb7496a9e929431cf258f7821
Dave Mathews <davenmathews@gmail.com>
2018-06-09 11:52:45 +0300

    [Chore #158234215] Rename 'register' to 'signup'
    [Finishes #158234215]
    
    Signed-off-by: Dave Mathews <davenmathews@gmail.com>

--- MaintenanceTrackerAPI/api/v1/auth/__init__.py
+++ MaintenanceTrackerAPI/api/v1/auth/__init__.py
@@ -2,12 +2,12 @@ from flask_restplus.namespace import Namespace
 
 from MaintenanceTrackerAPI.api.v1.auth.login import Login, user_login_model
 from MaintenanceTrackerAPI.api.v1.auth.logout import Logout
-from MaintenanceTrackerAPI.api.v1.auth.register import Register, \
+from MaintenanceTrackerAPI.api.v1.auth.signup import Register, \
     user_registration_model
 
 auth_ns = Namespace('auth', description='Operations related to authentication')
 
-auth_ns.add_resource(Register, '/register', endpoint='auth_register')
+auth_ns.add_resource(Register, '/signup', endpoint='auth_register')
 auth_ns.add_model('user_registration_model', user_registration_model)
 
 auth_ns.add_resource(Login, '/login')
